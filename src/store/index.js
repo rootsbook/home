0fe00464f3754f0e9442ea318f8bdee32d601fa1
rootsbook <rootsbook@163.com>
2025-05-05 18:42:29 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,12 @@ export const mainStore = defineStore("main", {
       musicClick: false, // 音乐链接是否跳转
       musicIsOk: false, // 音乐是否加载完成
       musicVolume: 0, // 音乐音量;
-      musicOpenState: false, // 音乐面板开启状态
-      backgroundShow: false, // 壁纸展示状态
+      musicOpenState: true, // 音乐面板开启状态
+      backgroundShow: true, // 壁纸展示状态
       boxOpenState: false, // 盒子开启状态
       mobileOpenState: true, // 移动端开启状态
       mobileFuncState: true, // 移动端功能区开启状态
-      setOpenState: true, // 设置页面开启状态
+      setOpenState: false, // 设置页面开启状态
       playerState: false, // 当前播放状态
       playerTitle: null, // 当前播放歌曲名
       playerArtist: null, // 当前播放歌手名
